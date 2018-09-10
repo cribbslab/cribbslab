98a50b5a513b8fe8babab3314ae8caf867e27338
Acribbs <adam.cribbs@imm.ox.ac.uk>
2018-09-10 09:44:12 +0100

    added cat fastq script to merge all fastq files in a directory

--- Pipelines/pipeline_cat_fastq.py
+++ Pipelines/pipeline_cat_fastq.py
@@ -1,5 +1,5 @@
 """===========================
-Pipeline template
+Pipeline cat_fastq
 ===========================
 
 .. Replace the documentation below with your own description of the
@@ -67,8 +67,9 @@ Code
 """
 import sys
 import os
-from ruffus import transform, regex, suffix, follows
+from ruffus import *
 from CGATCore import Pipeline as P
+import CGATCore.Experiment as E
 
 # load options from the config file
 PARAMS = P.get_parameters(
@@ -76,57 +77,32 @@ PARAMS = P.get_parameters(
      "../pipeline.yml",
      "pipeline.yml"])
 
-# add configuration values from associated pipelines
-#
-# 1. pipeline_genesets: any parameters will be added with the
-#    prefix "annotations_". The interface will be updated with
-#    "annotations_dir" to point to the absolute path names.
-PARAMS.update(P.peek_parameters(
-    PARAMS["annotations_dir"],
-    "pipeline_genesets.py",
-    "genesets",
-    on_error_raise=__name__ == "__main__",
-    prefix="annotations_",
-    update_interface=True))
 
-# ---------------------------------------------------
-# Specific pipeline tasks
-@transform(("pipeline.yml",),
-           regex("(.*)\.(.*)"),
-           r"\1.counts")
-def count_words(infile, outfile):
-    '''count the number of words in the pipeline configuration files.'''
+SEQUENCESUFFIXES = ("*.fastq.1.gz",
+                    "*.fastq.2.gz",
+                    "*.fastq.gz"
+                    )
 
-    # the command line statement we want to execute
-    statement = '''awk 'BEGIN { printf("word\\tfreq\\n"); } 
-    {for (i = 1; i <= NF; i++) freq[$i]++}
-    END { for (word in freq) printf "%%s\\t%%d\\n", word, freq[word] }'
-    < %(infile)s > %(outfile)s'''
+FASTQTARGET = tuple([os.path.join(".", suffix_name)
+                       for suffix_name in SEQUENCESUFFIXES])
 
-    # execute command in variable statement.
-    #
-    # The command will be sent to the cluster.  The statement will be
-    # interpolated with any options that are defined in in the
-    # configuration files or variable that are declared in the calling
-    # function.  For example, %(infile)s will we substituted with the
-    # contents of the variable "infile".
+@collate(FASTQTARGET,
+         regex("%s.fastq*" % PARAMS["merge_pattern_input"].strip()),
+         r"%s.fastq" % (PARAMS["merge_pattern_output"].strip()))
+def merge_fastq(infiles, outfile):
+    '''merge fastq files based on the merge pattern.'''
+
+    infiles = list(infiles)
+    infiles = " ".join(infiles)
+
+    statement = '''cat %(infiles)s > %(outfile)s'''
+
     P.run(statement)
 
 
-@transform(count_words,
-           suffix(".counts"),
-           "_counts.load")
-def load_word_counts(infile, outfile):
-    '''load results of word counting into database.'''
-    P.load(infile, outfile, "--add-index=word")
-
-
-# ---------------------------------------------------
-# Generic pipeline tasks
-@follows(load_word_counts)
+@follows(merge_fastq)
 def full():
     pass
-
 
 def main(argv=None):
     if argv is None:
