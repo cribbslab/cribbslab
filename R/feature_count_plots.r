d5a68b4cbbac4e21d355ecb7d8891b1edb323b0a
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2018-12-11 13:49:09 +0000

    updates

--- R/feature_count_plots.r
+++ R/feature_count_plots.r
@@ -1,20 +1,9 @@
 #!/usr/bin/Rscript
 
-#setwd(".")
 library("ggplot2")
 library("tidyverse")
 library("optparse")
-
-
-#setwd("/ifs/research-groups/botnar/proj025/analyses/test_trna_3")
-
-# Get number of samples and sample names from folders in featurecounts
-#sample_folders <- list.files(path= "featurecounts.dir/" , pattern = '')
-#num_samples = length(sample_folders)
-
-# Initialise empty 'tibble' to bind tibbles for each sample to
-#feature_count_tibble_all_samples = NULL
-
+library("ggpubr")
 
 option_list <- list(
 	    make_option(c("--input"), default="must_specify",
@@ -25,13 +14,13 @@ option_list <- list(
 #OptionParser(option_list=option_list)
 opt <- parse_args(OptionParser(option_list=option_list))
 
-
 print("Running with following options:")
 print(opt)
 
 #test_in <- "featurecounts.dir/test/test.feature_small.tsv:featurecounts.dir/test_2/test_2.feature_small.tsv"
 #test_out <- "plots.dir/feature_count.png"
 
+#input <- unlist(strsplit(test_in, ":"))
 input <- unlist(strsplit(opt$input, ":"))
 output <- opt$output
 
@@ -47,14 +36,41 @@ for(i in 1:num_samples){
   sample <- stringr::str_match(path, "/(.*?)/")[2]
   feature_count_tibble$Sample <- sample
   total_mapped <- sum(feature_count_tibble$NumberMapped)
-  feature_count_tibble$Proportion <- (feature_count_tibble$NumberMapped / total_mapped)
+  feature_count_tibble$Percentages <- 100*(feature_count_tibble$NumberMapped / total_mapped)
   feature_count_tibble_all_samples = bind_rows(feature_count_tibble_all_samples,feature_count_tibble)
 }
 fc_tibble_all_samples_nonzero <- dplyr::filter(feature_count_tibble_all_samples, NumberMapped != 0)
 
-feature_count_plot <- ggplot(data=fc_tibble_all_samples_nonzero, aes(x=Sample, y=Proportion, fill=`GeneID`)) +
-  geom_bar(stat="identity") + coord_flip()
+feature_count_plot <- ggplot(data=fc_tibble_all_samples_nonzero, aes(x=Sample, y=Percentages, fill=`GeneID`)) +
+  geom_bar(stat="identity") + coord_flip() 
+  
+feature_count_plot <- set_palette(feature_count_plot, "lancet")
 
 #print(feature_count_plot)
 
 ggsave(filename = output, plot = feature_count_plot)
+
+
+file_paths = list.files(pattern='_small.tsv$', recursive=TRUE, full.names = TRUE)
+num_samples <- length(file_paths)
+feature_count_tibble_all_samples = NULL
+
+for(i in 1:num_samples){
+  path <- file_paths[i]
+  feature_count_tibble <-read_tsv(path, col_names = FALSE, skip = 2)
+  colnames(feature_count_tibble) <- c("GeneID", "NumberMapped")
+  # Get sample name 
+  sample <- stringr::str_match(path, "/(.*?)/")[2]
+  feature_count_tibble$Sample <- sample
+  total_mapped <- sum(feature_count_tibble$NumberMapped)
+  feature_count_tibble$Percentages <- 100*(feature_count_tibble$NumberMapped / total_mapped)
+  feature_count_tibble_all_samples = bind_rows(feature_count_tibble_all_samples,feature_count_tibble)
+}
+fc_tibble_all_samples_nonzero <- dplyr::filter(feature_count_tibble_all_samples, NumberMapped != 0)
+
+feature_count_plot <- ggplot(data=fc_tibble_all_samples_nonzero, aes(x=Sample, y=Percentages, fill=`GeneID`)) +
+  geom_bar(stat="identity") + coord_flip() 
+
+feature_count_plot <- set_palette(feature_count_plot, "lancet")
+
+print(feature_count_plot)
