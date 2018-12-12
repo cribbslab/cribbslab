c5924d9a1dc3189ba9362a25f540116a25ee1652
Adam Cribbs <adam.cribbs@imm.ox.ac.uk>
2018-12-12 20:26:31 +0000

    added R script for diffbind and a dirty start for ATACseqQc

--- R/DiffBind.R
+++ R/DiffBind.R
@@ -1,4 +1,7 @@
-# This script performs the loading of peaksets from the 
+# This script performs the loading of peaksets for ChIP-seq or ATAC-seq using
+# the diffbind package. Counts and performed and the output of this is then saved
+# as a dba_object.Rdata so it can be loaded in an R session and further analysis
+# performed on the data
 
 setwd(".")
 
@@ -6,7 +9,10 @@ suppressPackageStartupMessages(library(optparse))
 
 option_list <- list(
 	    make_option(c("--design"), default="must_specify",
-	    help="This specifies the design file according to the design file for diffbind.")
+	    help="This specifies the design file according to the design file for diffbind."),
+	    make_option(c("--filepath"), default="must_specify",
+	                help="This specifies the file path to the input files. The PeakFiles, bamReads and bamControl will have the file path prepended to names")
+	    
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
@@ -16,8 +22,28 @@ print(opt)
 
 
 design <- opt$design
+filepath <- opt$filepath
 
+suppressPackageStartupMessages(library(DiffBind))
 
-save(peaks_control, peaks_treatment, file="ChIPseeker.Rdata")
+# Load the design file
+samples <- read.csv(design)
+names(samples)
 
+# Construct the dba object
+dba_object <- dba(sampleSheet = samples, dir = filepath) 
 
+dba_object
+
+# Plot the correlation heatmap sing peak caller score
+plot(dba_object)
+
+#Count reads
+dba_object <- dba.count(dba_object, summits=250)
+
+dba_object
+
+# Plot correlation heatmap based on the counts
+plot(dba_object)
+
+save(dba_object, file="dba_object.Rdata")
