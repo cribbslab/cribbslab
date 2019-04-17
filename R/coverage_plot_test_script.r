0c24c8846277002e73ccb5a0226f84180dc58f92
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2019-04-17 15:09:23 +0100

    adding file

--- R/coverage_plot_test_script.r
+++ R/coverage_plot_test_script.r
@@ -2,6 +2,8 @@
 
 library("tidyverse")
 library("ggpubr")
+
+# Generalised version, need to make standardised 
 setwd("/ifs/research-groups/botnar/proj025/analyses/test_trna_3")
 
 # Just using one sample, would need to make multiple plots for multiple files (dynamically scale)
@@ -10,7 +12,7 @@ pileup <- read_tsv(file = "post_mapping_bams.dir/test_pileup.tsv", skip = 3, col
 # Only subset of 5 clusters. Find out what top 50 means, top 50 differentially expressed--> where is this info located
 small_pileup <- pileup %>% dplyr::filter(`#CHROM` %in% head(unique(pileup$`#CHROM`),60))
 
-subset_pileup <- dplyr::select(small_pileup, c(`#CHROM`, POS, REF, ALT)) %>% rename(cluster = `#CHROM`, Base = REF)
+subset_pileup <- dplyr::select(small_pileup, c(`#CHROM`, POS, REF, ALT)) %>% dplyr::rename(cluster = `#CHROM`, Base = REF)
 subset_pileup$Base <- as.factor(subset_pileup$Base)
 subset_pileup$ALT <- as.factor(subset_pileup$ALT)
 
