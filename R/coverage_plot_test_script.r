05dec04615ccd97ddcc9702f00220d5cbcf8d6fc
Anna James-Bott <anna.james-bott@st-hildas.ox.ac.uk>
2019-04-30 14:10:13 +0100

    Top 50 clusters based on coverage plotted.

--- R/coverage_plot_test_script.r
+++ R/coverage_plot_test_script.r
@@ -4,7 +4,7 @@ library("tidyverse")
 library("ggpubr")
 
 # Generalised version, need to make standardised 
-setwd("/ifs/research-groups/botnar/proj025/analyses/test_trna_3")
+setwd("/ifs/research-groups/botnar/proj025/analyses/test_april/")
 
 # Just using one sample, would need to make multiple plots for multiple files (dynamically scale)
 pileup <- read_tsv(file = "post_mapping_bams.dir/test_pileup.tsv", skip = 3, col_names = TRUE)
@@ -16,9 +16,17 @@ subset_pileup <- dplyr::select(small_pileup, c(`#CHROM`, POS, REF, ALT)) %>% dpl
 subset_pileup$Base <- as.factor(subset_pileup$Base)
 subset_pileup$ALT <- as.factor(subset_pileup$ALT)
 
-coverage_plot <- ggplot(data = subset_pileup, aes(x= POS, y = cluster, color = Base)) + 
-  geom_point() +theme_bw() + labs(x= "Position in tRNA", y = "Cluster")
-  
-set_palette(coverage_plot, "lancet")
+# Rank clusters by abundance
+idx_stats <- read_tsv(file = "post_mapping_bams.dir/test_trna.idxstats", col_names = FALSE)
+colnames(idx_stats) <- c("cluster", "length", "abundance", "not_sure")
 
-# Need to convert to AA / anticodon / 5' vs 3' identifiers?? e.g. Gly-GCC-5'
+idx_stats_filt <-  arrange(idx_stats, desc(abundance)) %>% top_n(50, abundance)
+top_clusters <- idx_stats_filt$cluster
+
+subset_pileup_top <- subset_pileup %>% filter(cluster %in% top_clusters) 
+subset_pileup_top$cluster <- factor(subset_pileup_top$cluster, levels = rev(top_clusters))
+
+set_palette(coverage_plot, "lancet")
+coverage_plot <- ggplot(data = subset_pileup_top, aes(x= POS, y = cluster, color = Base)) + 
+  geom_point() +theme_bw() + labs(x= "Position in tRNA", y = "Cluster")
+print(coverage_plot)
