c95d332694856dc9b0c55eb0b20deddd92ce528c
Acribbs <a.p.cribbs@gmail.com>
2025-01-08 20:28:45 +0000

    Added ont QC pipeline for assesing quality

---
+++ cribbslab/pipeline_ontqc.py
@@ -0,0 +1,83 @@
+"""Pipeline for quality control of Oxford Nanopore and PacBio long-read sequencing data.
+
+This pipeline runs quality control tools including NanoPlot and LongQC on long-read
+sequencing data to assess read quality, length distributions, and other QC metrics.
+
+Configuration parameters are stored in pipeline.yml.
+"""
+
+import sys
+import os
+import sqlite3
+import pandas as pd
+
+from ruffus import *
+from cgatcore import pipeline as P
+from cgatcore import experiment as E
+
+# load options from the config file
+PARAMS = P.get_parameters(
+    ["%s/pipeline.yml" % os.path.splitext(__file__)[0],
+     "../pipeline.yml",
+     "pipeline.yml"])
+
+# Helper functions
+def get_fastq_files(infile):
+    """Return a list of fastq files from a input file or pattern."""
+    if isinstance(infile, str):
+        if os.path.isfile(infile):
+            return [infile]
+        else:
+            return glob.glob(infile)
+    return infile
+
+# Pipeline tasks
+@follows(mkdir("nanoplot"))
+@transform("*.fastq.gz",
+           regex(r"(\S+).fastq.gz"),
+           r"nanoplot/\1/NanoPlot-report.html")
+def runNanoPlot(infile, outfile):
+    """Run NanoPlot on input fastq files."""
+    outdir = os.path.dirname(outfile)
+    
+    statement = """NanoPlot 
+                  --fastq %(infile)s 
+                  --outdir %(outdir)s 
+                  --threads %(nanoplot_threads)s 
+                  --maxlength %(nanoplot_maxlength)s 
+                  --minlength %(nanoplot_minlength)s 
+                  %(nanoplot_options)s"""
+    
+    P.run(statement)
+
+@follows(mkdir("longqc"))
+@transform("*.fastq.gz",
+           regex(r"(\S+).fastq.gz"),
+           r"longqc/\1/\1_longqc_report.html")
+def runLongQC(infile, outfile):
+    """Run LongQC on input fastq files."""
+    outdir = os.path.dirname(outfile)
+    sample_name = os.path.basename(os.path.dirname(outfile))
+    
+    statement = """longQC.py sampleqc 
+                  -x %(longqc_preset)s 
+                  -o %(outdir)s 
+                  -s %(sample_name)s 
+                  -p %(longqc_threads)s 
+                  %(longqc_options)s 
+                  %(infile)s"""
+    
+    P.run(statement)
+
+@follows(runNanoPlot, runLongQC)
+def full():
+    """Run the full pipeline."""
+    pass
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    P.main(argv)
+
+if __name__ == "__main__":
+    sys.exit(P.main(sys.argv))
